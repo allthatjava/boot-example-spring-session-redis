c81cb3016156eb0b9b3ecf4cd26769991d4e4901
Brian Heo <allthatjava@yahoo.ca>
2020-03-13 16:49:49 -0400

    Initial commit

---
+++ src/main/java/brian/boot/example/spring/session/redis/BootExampleSpringSessionRedisApplication.java
@@ -0,0 +1,13 @@
+package brian.boot.example.spring.session.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootExampleSpringSessionRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootExampleSpringSessionRedisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/brian/boot/example/spring/session/redis/bootexamplespringsessionredis/BootExampleSpringSessionRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package brian.boot.example.spring.session.redis.bootexamplespringsessionredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootExampleSpringSessionRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
